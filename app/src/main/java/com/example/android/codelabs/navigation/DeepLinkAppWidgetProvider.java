923c7ea4e67a10882d5dcef9adeb1e6fc3dd8744
Miklos Nemeth <nemethmiklos1960@gmail.com>
2019-08-25 12:30:18 +0200

    10. Deep Linking to a destination

--- app/src/main/java/com/example/android/codelabs/navigation/DeepLinkAppWidgetProvider.java
+++ app/src/main/java/com/example/android/codelabs/navigation/DeepLinkAppWidgetProvider.java
@@ -1,5 +1,6 @@
 package com.example.android.codelabs.navigation;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
@@ -7,12 +8,21 @@ import android.os.Bundle;
 import android.widget.RemoteViews;
 
 import androidx.annotation.NonNull;
+import androidx.navigation.NavDeepLinkBuilder;
 
 public final class DeepLinkAppWidgetProvider extends AppWidgetProvider {
    public void onUpdate(@NonNull Context context, @NonNull AppWidgetManager appWidgetManager, @NonNull int[] appWidgetIds) {
       RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.deep_link_appwidget);
       Bundle args = new Bundle();
       args.putString("myarg", "From Widget");
+      // TODO Step 10 - construct and set a PendingIntent using DeepLinkBuilder
+      PendingIntent pendingIntent = new NavDeepLinkBuilder(context)
+          .setGraph(R.navigation.mobile_navigation)
+          .setDestination(R.id.deeplink_dest)
+          .setArguments(args)
+          .createPendingIntent();
+      remoteViews.setOnClickPendingIntent(R.id.deep_link_button, pendingIntent);
+      // TODO END STEP 10
       appWidgetManager.updateAppWidget(appWidgetIds, remoteViews);
    }
 }
